be77cec554f0841d5ab87eca5967edb3e495b8c5
mvdbastos <mvdbastos@hotmail.com>
2022-01-02 01:25:33 -0300

    aula 4

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import Head from 'next/head'
-import {Entity, Scene} from 'aframe-react';
+import { Entity, Scene } from 'aframe-react';
 
 export default class extends React.Component {
   constructor(props) {
@@ -30,41 +30,35 @@ export default class extends React.Component {
     return (
       <div style={{ height: '100%', width: '100%' }}>
         <Head>
-            <meta charset="utf-8" />
-            <meta name="viewport" content="width=device-width, initial-scale=1" />
-            <title>A-Frame React Boilerplate</title>
+          <meta charset="utf-8" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+          <title>A-Frame React Boilerplate</title>
         </Head>
 
         {this.state.appRendered &&
-          <Scene>
-            <a-assets>
-              <img id="groundTexture" src="https://cdn.aframe.io/a-painter/images/floor.jpg"/>
-              <img id="skyTexture" src="https://cdn.aframe.io/a-painter/images/sky.jpg"/>
-            </a-assets>
-
-            <Entity primitive="a-plane" src="#groundTexture" rotation="-90 0 0" height="100" width="100"/>
-            <Entity primitive="a-light" type="ambient" color="#445451"/>
-            <Entity primitive="a-light" type="point" intensity="2" position="2 4 4"/>
-            <Entity primitive="a-sky" height="2048" radius="30" src="#skyTexture" theta-length="90" width="2048"/>
-            <Entity particle-system={{preset: 'snow', particleCount: 2000}}/>
-            <Entity text={{value: 'Hello, A-Frame React!', align: 'center'}} position={{x: 0, y: 2, z: -1}}/>
-
-            <Entity id="box"
-              geometry={{primitive: 'box'}}
-              material={{color: this.state.color, opacity: 0.6}}
-              animation__rotate={{property: 'rotation', dur: 2000, loop: true, to: '360 360 360'}}
-              animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '1.1 1.1 1.1'}}
-              position={{x: 0, y: 1, z: -3}}
-              events={{click: this.changeColor.bind(this)}}>
-              <Entity animation__scale={{property: 'scale', dir: 'alternate', dur: 100, loop: true, to: '2 2 2'}}
-                      geometry={{primitive: 'box', depth: 0.2, height: 0.2, width: 0.2}}
-                      material={{color: '#24CAFF'}}/>
-            </Entity>
-
-            <Entity primitive="a-camera">
-              <Entity primitive="a-cursor" animation__click={{property: 'scale', startEvents: 'click', from: '0.1 0.1 0.1', to: '1 1 1', dur: 150}}/>
-            </Entity>
-          </Scene>
+          <a-scene>
+            <a-box color="#689f38"
+              width="8"
+              height="4.5"
+              depth="0.2"
+              position="0 3 -7">
+              <a-circle color="#333333"
+                side="double"
+                position="2 0 0.11">
+              </a-circle>
+            </a-box>
+            <a-circle color="#ffc107"
+              side="double"
+              position="-2 3 -6.89">
+            </a-circle>
+            <a-cylinder color="#616161"
+              height="2"
+              radius="2"
+              segments-radial="6"
+              open-ended="true"
+              side="double">
+            </a-cylinder>
+          </a-scene>
         }
       </div>
     )
